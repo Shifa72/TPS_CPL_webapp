de3e74948087866f1d6f43b64bbc34929547cc1d
IS <shagbanov72@mail.ru>
2025-06-03 10:43:38 +0500

    fix: correct path to main.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/TPS_CPL_webapp/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true
+  }
 })
